9532de4fec2aa75886faeb28e76339b8456b4d34
Kevin Mazariegos <keedgmz19@gmail.com>
2022-02-10 12:47:54 -0600

    2.1do Despliegue en Heroku

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose      =        require('mongoose')
 const app           =        require('./config/app')
 const config        =        require('./config/config')
 
+
 mongoose.connect(config.conectdb.db, (err, res) => {
 
     if(err){
@@ -11,8 +12,8 @@ mongoose.connect(config.conectdb.db, (err, res) => {
     }
     console.log('La conexion a la base de datos ha sido exitosa')
 
-    app.listen(config.port, config.host, () =>  {
-        console.log(`API REST corriendo en http://${config.host}:${config.port}`)
+    app.listen(config.port, () =>  {
+        console.log(`API REST corriendo en http://localhost:${config.port}`)
     })
 
 })
